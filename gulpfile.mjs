7367ffa753b5f4d80db498520fae04d761e5714f
gretmn102 <erlgreywatson@gmail.com>
2025-06-12 23:32:14 +0300

    refactor: make build via gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,12 +1,51 @@
 // @ts-check
 import fs from "fs"
 import { task, src, dest } from "gulp"
+import log from "gulplog"
+import path from "path"
+import ts from "typescript"
 import jsonModifier from "gulp-json-modifier"
 
 const buildPath = "./build"
 
 task("clear", cb => {
   fs.rm(`${buildPath}`, { recursive: true, force: true }, cb)
+})
+
+task("build", done => {
+  const configPath = "tsconfig.json"
+  const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
+  const parsedCommandLine = ts.parseJsonConfigFileContent(
+    configFile.config,
+    ts.sys,
+    path.dirname(configPath)
+  )
+
+  const program = ts.createProgram(
+    parsedCommandLine.fileNames,
+    parsedCommandLine.options
+  )
+
+  const emitResult = program.emit()
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics)
+
+  if (allDiagnostics.length > 0) {
+    allDiagnostics.forEach(diagnostic => {
+      const message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      )
+      log.error(`Error ${diagnostic.code}: ${message}`)
+    })
+    done(new Error("Compilation error!"))
+    return
+  }
+
+  log.info("Compilation successful!")
+  done()
 })
 
 task("packageJsonCopy", cb => {
