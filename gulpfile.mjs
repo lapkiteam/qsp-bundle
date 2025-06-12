c861865200e3fe58e82c3904539f6cf5e0c871de
gretmn102 <erlgreywatson@gmail.com>
2025-06-12 20:06:35 +0300

    refactor: make clear via gulp

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,6 +1,13 @@
 // @ts-check
+import fs from "fs"
 import { task, src, dest } from "gulp"
 import jsonModifier from "gulp-json-modifier"
+
+const buildPath = "./build"
+
+task("clear", cb => {
+  fs.rm(`${buildPath}`, { recursive: true, force: true }, cb)
+})
 
 task("packageJsonCopy", cb => {
   src("./package.json")
@@ -9,6 +16,6 @@ task("packageJsonCopy", cb => {
       delete json.devDependencies
       return json
     }))
-    .pipe(dest("./build"))
+    .pipe(dest(buildPath))
     .on("end", cb)
 })
