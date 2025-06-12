ae762efbb8187f225f9ed5c300df81844aecac2f
gretmn102 <erlgreywatson@gmail.com>
2025-06-12 19:59:12 +0300

    wip: make package.json modifier and copy

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,12 +1,14 @@
 // @ts-check
-import { task, watch, series } from "gulp"
+import { task, src, dest } from "gulp"
+import jsonModifier from "gulp-json-modifier"
 
-task("watch", (cb) => {
-  watch("src/*")
-    .on("change", (path) => {
-      console.log(`${path} changed...`)
-      cb()
-    })
+task("packageJsonCopy", cb => {
+  src("./package.json")
+    .pipe(jsonModifier(function(json) {
+      delete json.scripts
+      delete json.devDependencies
+      return json
+    }))
+    .pipe(dest("./build"))
+    .on("end", cb)
 })
-
-task("default", series("watch"))
