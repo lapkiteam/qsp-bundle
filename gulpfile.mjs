a1913fc5518a773cc8721f0c169f1db68672d155
Fering <treesortbubble@gmail.com>
2025-06-14 23:48:01 +0300

    feat: setup environment (#1)
    * wip
    
    * wip
    
    * wip: add gulp
    
    * wip: make package.json modifier and copy
    
    * refactor: make clear via gulp
    
    * refactor: make build via gulp
    
    * wip: generate declaration on
    
    * refactor: make deploy via gulp
    
    * refactor: rename "clear" to "clean"
    
    * wip: add `readmeCopy` task
    
    * wip: make CLI
    
    * wip: remove `other.ts`
    
    * refactor: rename `app.ts` to `index.ts`

---
+++ gulpfile.mjs
@@ -0,0 +1,77 @@
+// @ts-check
+import fs from "fs"
+import { task, src, dest, series } from "gulp"
+import log from "gulplog"
+import path from "path"
+import ts from "typescript"
+import jsonModifier from "gulp-json-modifier"
+
+const buildPath = "./build"
+
+task("clean", cb => {
+  fs.rm(`${buildPath}`, { recursive: true, force: true }, cb)
+})
+
+task("build", done => {
+  const configPath = "tsconfig.json"
+  const configFile = ts.readConfigFile(configPath, ts.sys.readFile)
+  const parsedCommandLine = ts.parseJsonConfigFileContent(
+    configFile.config,
+    ts.sys,
+    path.dirname(configPath)
+  )
+
+  const program = ts.createProgram(
+    parsedCommandLine.fileNames,
+    parsedCommandLine.options
+  )
+
+  const emitResult = program.emit()
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics)
+
+  if (allDiagnostics.length > 0) {
+    allDiagnostics.forEach(diagnostic => {
+      const message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      )
+      log.error(`Error ${diagnostic.code}: ${message}`)
+    })
+    done(new Error("Compilation error!"))
+    return
+  }
+
+  log.info("Compilation successful!")
+  done()
+})
+
+task("packageJsonCopy", cb => {
+  src("./package.json")
+    .pipe(jsonModifier(function(json) {
+      delete json.scripts
+      delete json.devDependencies
+      json.main = "bin/qsp-bundle.js"
+      json.bin = {
+        ["qsp-bundle"]: "bin/qsp-bundle.js"
+      }
+      return json
+    }))
+    .pipe(dest(buildPath))
+    .on("end", cb)
+})
+
+task("readmeCopy", done => {
+  src("./README.md")
+    .pipe(dest(buildPath))
+    .on("end", done)
+})
+
+task("deploy", series([
+  "clean",
+  "build",
+  "readmeCopy",
+  "packageJsonCopy",
+]))
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { parse } from "@typescript-eslint/parser"
+
+import { hello } from "./lib"
+
+hello()
+const x = new Set()
+console.log(parse("hello"))
