ab3b9d50eb4461b8417798fae26c5707413324d7
gretmn102 <erlgreywatson@gmail.com>
2025-06-15 20:25:48 +0300

    feat: make `bundle` function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import { parse } from "@typescript-eslint/parser"
+import { bundle, Options } from "./lib"
 
-import { hello } from "./lib"
-
-hello()
-const x = new Set()
-console.log(parse("hello"))
+const act = bundle("../tests/mockGame/main.qsps")
+const exp = [
+  "# main\r\n\r\n-\r\n",
+  "# lib\r\n\r\n-\r\n",
+  "# version\r\n\r\n-\r\n",
+  "# hero\r\n\r\n-\r\n",
+  "# potion\r\n\r\n-\r\n",
+  "# shield\r\n\r\n-\r\n",
+  "# sword\r\n\r\n-\r\n",
+  "# city\r\n\r\n-\r\n",
+  "# forest\r\n\r\n-\r\n",
+].join(Options.separatorDefault)
+console.log(act === exp)
