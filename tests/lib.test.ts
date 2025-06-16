4140ec295425ca369c6604e96d93f913161e3bae
gretmn102 <erlgreywatson@gmail.com>
2025-06-16 23:02:17 +0300

    fix(test): `\r\n`

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -21,15 +21,15 @@ describe("bundle", () => {
         ]
       }
       return [
-        "# main\r\n\r\n-\r\n",
-        "# lib\r\n\r\n-\r\n",
-        "# version\r\n\r\n-\r\n",
-        "# hero\r\n\r\n-\r\n",
-        "# city\r\n\r\n-\r\n",
-        "# forest\r\n\r\n-\r\n",
-        "# sword\r\n\r\n-\r\n",
-        "# potion\r\n\r\n-\r\n",
-        "# shield\r\n\r\n-\r\n",
+        "# main\n\n-\n",
+        "# lib\n\n-\n",
+        "# version\n\n-\n",
+        "# hero\n\n-\n",
+        "# city\n\n-\n",
+        "# forest\n\n-\n",
+        "# sword\n\n-\n",
+        "# potion\n\n-\n",
+        "# shield\n\n-\n",
       ]
     }
     )().join(Options.separatorDefault))
