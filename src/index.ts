214f0dc77b1dee8c228cfbe57dd360463699d536
gretmn102 <erlgreywatson@gmail.com>
2025-06-16 22:19:50 +0300

    feat(cli): add command

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
+import { Command } from "commander"
+import { writeFileSync } from "fs"
+
 import { bundle } from "./lib"
 
-console.log(bundle("../tests/mockGame/main.qsps"))
+const program = new Command()
+
+program
+  .name("qsp-bundle")
+  .description("Собирает воедино все QSP исходники в папках и в подпапках согласно указанному главному исходнику.")
+  .version("0.1.0")
+
+program
+  .argument("<main_source_path>", "string to split")
+  .option("-o, --output <path>", "output path")
+  .action((mainSourcePath, options) => {
+    const result = bundle(mainSourcePath)
+    const outputPath: string | undefined = options.output
+    if (!outputPath) {
+      console.log(result)
+      return
+    }
+    writeFileSync(outputPath, result)
+  })
+
+program.parse()
