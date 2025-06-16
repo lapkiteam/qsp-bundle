be1e7f2c347cf3a3c50e48938ba3927b2936dacc
gretmn102 <erlgreywatson@gmail.com>
2025-06-16 22:58:49 +0300

    fix(test)

--- tests/lib.test.ts
+++ tests/lib.test.ts
@@ -6,16 +6,32 @@ describe("bundle", () => {
   it("mockgame", () => {
     expect(
       bundle("./tests/mockGame/main.qsps")
-    ).toStrictEqual([
+    ).toStrictEqual((() => {
+      if (process.platform === "win32") {
+        return [
+          "# main\r\n\r\n-\r\n",
+          "# lib\r\n\r\n-\r\n",
+          "# version\r\n\r\n-\r\n",
+          "# hero\r\n\r\n-\r\n",
+          "# potion\r\n\r\n-\r\n",
+          "# shield\r\n\r\n-\r\n",
+          "# sword\r\n\r\n-\r\n",
+          "# city\r\n\r\n-\r\n",
+          "# forest\r\n\r\n-\r\n",
+        ]
+      }
+      return [
         "# main\r\n\r\n-\r\n",
         "# lib\r\n\r\n-\r\n",
         "# version\r\n\r\n-\r\n",
         "# hero\r\n\r\n-\r\n",
-        "# potion\r\n\r\n-\r\n",
-        "# shield\r\n\r\n-\r\n",
-        "# sword\r\n\r\n-\r\n",
         "# city\r\n\r\n-\r\n",
         "# forest\r\n\r\n-\r\n",
-      ].join(Options.separatorDefault))
+        "# sword\r\n\r\n-\r\n",
+        "# potion\r\n\r\n-\r\n",
+        "# shield\r\n\r\n-\r\n",
+      ]
+    }
+    )().join(Options.separatorDefault))
   })
 })
